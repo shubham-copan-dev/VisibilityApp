cd515aabf1561ae62600ae877bf1f88f2a9085e1
Shubham-dev-copan <shubham.shrivastav@copancs.com>
2023-12-22 17:26:47 +0530

    creating revenue card static

--- App.tsx
+++ App.tsx
@@ -1,36 +1,45 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StatusBar, Image} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  ScrollView,
+} from 'react-native';
 import styles from './styles/styles';
 import PaymentCardView from './components/PaymentCard';
 import GoalAttainment from './components/GoalAttainment';
-// import RevenueCompositionCard from './components/RevenueCompositionCard';
+import RevenueCompositionCard from './components/RevenueCompositionCard';
 
 function App(): React.JSX.Element {
   return (
-    <View>
-      <StatusBar backgroundColor="#EFF5F9" barStyle="dark-content" />
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.HeaderContainerView}>
-          <View>
-            <Text style={styles.homeText}>Home</Text>
-            <Text style={styles.updatedText}>Updated 11 hrs ago</Text>
+    <ScrollView>
+      <View>
+        <StatusBar backgroundColor="#EFF5F9" barStyle="dark-content" />
+        <SafeAreaView style={styles.safeArea}>
+          <View style={styles.HeaderContainerView}>
+            <View>
+              <Text style={styles.homeText}>Home</Text>
+              <Text style={styles.updatedText}>Updated 11 hrs ago</Text>
+            </View>
+            <View style={styles.iconsContainer}>
+              <Image
+                source={require('./assets/images/Outline.png')}
+                alt="Settings-icon"
+              />
+              <Image
+                source={require('./assets/images/Ellipse.png')}
+                alt="profile-icon"
+              />
+            </View>
           </View>
-          <View style={styles.iconsContainer}>
-            <Image
-              source={require('./assets/images/Outline.png')}
-              alt="Settings-icon"
-            />
-            <Image
-              source={require('./assets/images/Ellipse.png')}
-              alt="profile-icon"
-            />
-          </View>
-        </View>
-        <PaymentCardView />
-        <GoalAttainment />
-        {/* <RevenueCompositionCard /> */}
-      </SafeAreaView>
-    </View>
+          <PaymentCardView />
+          <GoalAttainment />
+          <RevenueCompositionCard />
+        </SafeAreaView>
+      </View>
+    </ScrollView>
   );
 }
 
--- components/RevenueCompositionCard.tsx
+++ components/RevenueCompositionCard.tsx
@@ -1,38 +1,103 @@
-// import React from 'react';
-// import {View, Image, Text} from 'react-native';
-// import {StyleSheet} from 'react-native';
+import React from 'react';
+import {View, Image, Text} from 'react-native';
+import {StyleSheet} from 'react-native';
 
-// const styles = StyleSheet.create({
-//   });
+const styles = StyleSheet.create({
+  revenueCardContainer: {
+    backgroundColor: '#fff',
+    marginTop: 32,
+    borderRadius: 18,
+    paddingTop: 26,
+    paddingBottom: 26,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  titleLabel: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 15,
+  },
+  label: {
+    color: '#252B41',
+    fontFamily: 'Poppins',
+    fontSize: 16,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 20.8,
+  },
+  totalRevenueCost: {
+    fontSize: 30,
+    fontStyle: 'normal',
+    fontWeight: '400',
+    lineHeight: 30 * 1.3,
+    color: '#252B41',
+  },
+  percentageDetail: {
+    color: '#252B41', // Default color if variable is not defined
+    fontVariant: ['lining-nums', 'tabular-nums', 'slashed-zero'],
+    fontFeatureSettings: "'clig' off, 'liga' off",
+    fontFamily: 'Poppins',
+    fontSize: 12,
+    fontStyle: 'normal',
+    fontWeight: '500',
+    lineHeight: 12 * 1.3, // Adjust as needed based on your percentage calculation
+  },
+  graphContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  graphStatus: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
+  },
+});
 
-// const RevenueCompositionCard = () => {
-//   return (
-//     <View>
-//       <View>
-//         <View>
-//           <Image
-//             source={require('../assets/images/chevron-right-icon.png')}
-//             alt="right-icon"
-//           />
-//           <Text>$ Renewed</Text>
-//         </View>
-//         <Image
-//           source={require('../assets/images/chevron-right-icon.png')}
-//           alt="right-icon"
-//         />
-//       </View>
-//       <Text>24M (65.5% of total Revenue)</Text>
-//       <View>
-//         <View>
-//           <Text>Software {'\n'} 6.5M</Text>
-//         </View>
-//         <View>
-//           <Text>Cross-Sell {'\n'} 4.2M</Text>
-//         </View>
-//         <Text>Graph img</Text>
-//       </View>
-//     </View>
-//   );
-// };
+const RevenueCompositionCard = () => {
+  return (
+    <>
+      <View style={styles.revenueCardContainer}>
+        <View style={styles.titleContainer}>
+          <View style={styles.titleLabel}>
+            <Image
+              source={require('../assets/images/BoldDuotone.png')}
+              alt="right-icon"
+            />
+            <Text style={styles.label}>$ Renewed</Text>
+          </View>
+          <Image
+            source={require('../assets/images/chevron-right-icon.png')}
+            alt="right-icon"
+          />
+        </View>
+        <View style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
+          <Text style={styles.totalRevenueCost}>24M</Text>
+          <Text style={styles.percentageDetail}>(65.5% of total Revenue)</Text>
+        </View>
+        <View style={styles.graphContainer}>
+          <View style={styles.graphStatus}>
+            <View>
+              <Text>Software {'\n'} 16M</Text>
+            </View>
+            <View>
+              <Text>Cross-Sell {'\n'} 8M</Text>
+            </View>
+          </View>
+          <Image
+            source={require('../assets/images/Donut2.png')}
+            alt="right-icon"
+            style={{width: 50, height: 50}}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
 
-// export default RevenueCompositionCard;
+export default RevenueCompositionCard;
